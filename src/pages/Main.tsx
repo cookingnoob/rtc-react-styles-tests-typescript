e6d31dbcbf09ab0f193e3b9c066efaa23a9afe50
cookingnoob <costealan@gmail.com>
2024-02-03 12:27:59 -0600

    added all the elements of the  main components, still need styles

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,27 @@
-import { Button, IconButton, Input, Typography } from '@mui/material'
+import { Box, Button, IconButton, Input, Link, Typography } from '@mui/material'
 import React from 'react'
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+
+//estilos acorde a la imagen de referencia
+//interactivilidad 
+  //mandas tu correo
+    //el boton se vuelve en un spinner
+    //mensaje de que recibido
+  //boton de ver el video se abre un modal con un vide
+  //boton de mas info se abre una pagina extra
 
 const Main = () => {
   return (
     <>
+    <Box display={'flex'} 
+          alignContent={'center'} 
+          justifyContent={'center'}>
+      <Box >
+       <img src='../../public/mainImage.png' 
+            alt='Imagen de la propiedad'/>
+     </Box>
+
+      <Box>
       <Typography variant='h2'>Siéntete en tu casa allá donde te lleve tu trabajo</Typography>
       <Typography variant='body1'>
         echale un vistazo al video para viajes de trabajo 
@@ -14,7 +31,13 @@ const Main = () => {
       placeholder='Escribe tu correo'
      />
      <Button>Agregar correo electrónico</Button>
-     <Typography>¿Haces viajes para tu empresa?</Typography>
+     <Typography>¿Haces viajes para tu empresa?
+        <Link>Más información</Link>
+        <IconButton aria-label='Más información'> <ArrowRightIcon/> </IconButton>
+     </Typography>
+     </Box>
+    </Box>
+ 
     </>
   )
 }
