a943cb4f696e262e39634a65149f0dc2390acb6e
cookingnoob <costealan@gmail.com>
2024-02-04 13:00:50 -0600

    added interactivity to the form elements

---
+++ src/components/MailForm.tsx
@@ -0,0 +1,52 @@
+import { Button, Input, Typography, css } from '@mui/material'
+import React, { useState } from 'react'
+
+const inputStyles = css`
+background: white;
+border-radius: 0px;
+width: 300px
+`
+const submitButtonStyles = css`
+background: rgba(58, 102, 61, .6);
+color: whitesmoke;
+
+&:hover{
+  background: rgba(123, 227, 130, 1);
+
+}
+`
+const MailForm = () => {
+  const [isDisabled, setIsDisabled] = useState(false)
+  const [userInput, setUserInput] = useState('')
+  const [wrongInput, setWrongInput] = useState(false)
+
+  const handleInput = (event) => {
+    setUserInput(`${event.target.value}`)
+  }
+
+  const handleSubmit = () => {
+    if(userInput === '') {
+      setWrongInput(true)
+      return
+    } 
+    setIsDisabled(true)
+  }
+
+  return (
+    <>
+    {isDisabled ? 
+    <>
+    <Typography variant='h5'>Te notificaremos a {userInput} de las actualizaciones</Typography>
+    </> : 
+    
+    <>
+       <Typography>para empezar agrega tu correo electónico de trabajo</Typography>
+       <Input size='medium' color='primary' sx={inputStyles} onChange={handleInput}/>
+    </>}
+       <Button sx={submitButtonStyles} onClick={() => handleSubmit()} disabled={isDisabled}>Agregar correo electrónico</Button> 
+       {wrongInput && <Typography>Algo está mal en el campo del correo</Typography>}
+    </>
+  )
+}
+
+export default MailForm
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, IconButton, Input, Link, Typography } from '@mui/material'
+import { Box, Button, IconButton, Input, Link, Typography, css } from '@mui/material'
 import React from 'react'
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import MailForm from '../components/MailForm';
 
 //estilos acorde a la imagen de referencia
 //interactivilidad 
@@ -9,28 +10,30 @@ import ArrowRightIcon from '@mui/icons-material/ArrowRight';
     //mensaje de que recibido
   //boton de ver el video se abre un modal con un vide
   //boton de mas info se abre una pagina extra
-
 const Main = () => {
   return (
     <>
     <Box display={'flex'} 
-          alignContent={'center'} 
-          justifyContent={'center'}>
-      <Box >
+          height={'90vh'}>
+      <Box position={'absolute'}
+           zIndex={0}
+        >
        <img src='../../public/mainImage.png' 
             alt='Imagen de la propiedad'/>
      </Box>
 
-      <Box>
-      <Typography variant='h2'>Siéntete en tu casa allá donde te lleve tu trabajo</Typography>
+      <Box zIndex={1}
+          color={'whitesmoke'}
+          width={'500px'}
+          marginLeft={'100px'}
+          marginTop={'200px'}>
+
+      <Typography variant='h4'>Siéntete en casa allá donde te lleve tu trabajo</Typography>
       <Typography variant='body1'>
-        echale un vistazo al video para viajes de trabajo 
-         <IconButton aria-label='Ver video'> <ArrowRightIcon/> </IconButton>
+        Échale un vistazo al video para viajes de trabajo 
+         <IconButton aria-label='Ver video' > <ArrowRightIcon/> </IconButton>
       </Typography>
-     <Input
-      placeholder='Escribe tu correo'
-     />
-     <Button>Agregar correo electrónico</Button>
+      <MailForm/>
      <Typography>¿Haces viajes para tu empresa?
         <Link>Más información</Link>
         <IconButton aria-label='Más información'> <ArrowRightIcon/> </IconButton>
